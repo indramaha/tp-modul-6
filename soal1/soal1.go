6b0c008bca9272f21d110e874eabdc4709f986bf
Tugus <idabgusindra01@gmail.com>
2023-04-01 14:49:11 +0800

    on progress question 1

--- soal1/soal1.go
+++ soal1/soal1.go
@@ -1,16 +1,28 @@
+//jawaban dari chat GPT, nomor 1 belum tau cara pastinya
+
 package main
 
 import "fmt"
 
-// func abc_1304221045(x int) string{
-// 	var s string
-
-// }
+func abc_1304221045(x int) string {
+	var s string
+    if x == 0 {
+        return ""
+    } else {
+        s = abc_1304221045(x / 2)
+        if x % 2 == 0 {
+            return s + "0"
+        } else {
+            return s + "1"
+        }
+    }
+}
 
 func main() {
-	var x, hasil int
+	var x int
+	var hasil string
 
 	fmt.Scan(&x)
-	hasil = x/2
+	hasil = abc_1304221045(x)
 	fmt.Println(hasil)
 }
